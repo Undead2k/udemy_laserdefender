8b456fdf3a187e7e457b3f0b679b12468224a217
Undead2k <addie2k3@hotmail.com>
2015-11-24 17:12:11 +0000

    The Great Curve
    Re-organised my folders.
    Added enemy and spots for it to spawn in.
    
    Now working out of the course to investigate SPLINES and pathing. I want
    my ships to move along paths ;D

---
+++ project/Assets/Entities/EnemyFormation/Enemy_Spawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Spawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	float width = 15f;
+	float height = 10f;
+
+	// Use this for initialization
+	void Start () {
+
+
+		foreach (Transform child in transform) {
+		
+			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		
+
+
+		}
+	}
+
+	public void OnDrawGizmos(){
+
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0));
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- project/Assets/Entities/PlayerShip/PlayerController.cs
+++ project/Assets/Entities/PlayerShip/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour {
 	float clamp_maxx;
 	float clamp_miny;
 	float clamp_maxy;
-
+	float padding = 0.5f;
 
 
 	// Use this for initialization
@@ -19,50 +19,58 @@ public class PlayerController : MonoBehaviour {
 	
 		playerPos = new Vector3 (0f, 0f, 0f);
 
-		float distance = transform.position.z - Camera.main.transform.position.z;
-		Vector3 bottom_left = Camera.main.ViewportToWorldPoint (new Vector3 (0,0,distance));
-		Vector3 bottom_right = Camera.main.ViewportToWorldPoint (new Vector3 (1,1,distance));
-		//Vector3 top_left = Camera.main.ViewportToWorldPoint (new Vector3 (0,1,distance));
-		//Vector3 top_right = Camera.main.ViewportToWorldPoint (new Vector3 (1,1,distance));
+		PlaySpaceBounderies ();
 
-		clamp_minx = bottom_left.x;
-		clamp_maxx = bottom_right.x;
-		//clamp_miny = top_left.y;
-		//clamp_maxy = top_right.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		PlayerMover1 ();
-	}
 
+
+	}
 
 	void PlayerMover1(){
-
+		
 		if (Input.GetKey ("up")) {
 			playerPos.y += speed * Time.deltaTime;
-
-
+			
+			
 		}  if (Input.GetKey ("down")) {
 			playerPos.y -= speed * Time.deltaTime;
-
-
+			
+			
 		}  if (Input.GetKey ("left")) {
 			playerPos.x -= speed * Time.deltaTime;
-
-
+			
+			
 		}  if (Input.GetKey ("right")) {
 			playerPos.x += speed * Time.deltaTime;
-
+			
 		} 
-
-		//this.transform.position = playerPos;
-
-		RestrictPlayer ();
-
+		
+		this.transform.position = playerPos;
+		
+		RestrictPlayerMovement ();
+		
 	}
 
-	void RestrictPlayer(){
+	void PlaySpaceBounderies(){
+
+		float distance = transform.position.z - Camera.main.transform.position.z;
+
+		Vector3 left = Camera.main.ViewportToWorldPoint (new Vector3 (0,0,distance));
+		Vector3 right = Camera.main.ViewportToWorldPoint (new Vector3 (1,0,distance));
+		Vector3 bottom = Camera.main.ViewportToWorldPoint (new Vector3 (0,0,distance));
+		Vector3 top = Camera.main.ViewportToWorldPoint (new Vector3 (0,1,distance));
+		
+		clamp_minx = left.x + padding;
+		clamp_maxx = right.x - padding;
+		clamp_miny = bottom.y + padding;
+		clamp_maxy = top.y - padding;
+	}
+
+	void RestrictPlayerMovement(){
 
 		float wallX = Mathf.Clamp (playerPos.x, clamp_minx, clamp_maxx);
 		float wallY = Mathf.Clamp (playerPos.y, clamp_miny, clamp_maxy);
