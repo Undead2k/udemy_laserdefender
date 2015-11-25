c85e5a573708bf03ecdc9b6afc5f8fac74243e0a
Undead2k <addie2k3@hotmail.com>
2015-11-25 16:59:33 +0000

    Spline fun
    More fun with the spline tutorial

--- project/Assets/Editor/CurveInspector.cs
+++ project/Assets/Editor/CurveInspector.cs
@@ -11,6 +11,8 @@ public class CurveInspector : Editor {
 
 	private const int lineSteps = 10;
 
+	private const float directionScale = 0.5f;
+
 	private void OnSceneGUI(){
 		curve = target as Curve;
 			
@@ -22,20 +24,46 @@ public class CurveInspector : Editor {
 		Vector3 p0 = ShowPoint (0);
 		Vector3 p1 = ShowPoint (1);
 		Vector3 p2 = ShowPoint (2);
+		Vector3 p3 = ShowPoint (3);
 			
 		Handles.color = Color.gray;
 		Handles.DrawLine (p0, p1);
-		Handles.DrawLine (p1, p2);
+		Handles.DrawLine (p2, p3);
 
-		Handles.color = Color.white;
-		Vector3 lineStart = curve.GetPoint (0f);
-		for (int i = 1; i <= lineSteps; i++) {
-			Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
-			Handles.DrawLine(lineStart, lineEnd);
-			lineStart = lineEnd;
-		}
-			
+		ShowDirections ();
+		Handles.DrawBezier (p0, p3, p1, p2, Color.white, null, 2f);
+
 	}
+
+	private void ShowDirections() {
+
+		Handles.color = Color.green;
+		Vector3 point = curve.GetPoint (0f);
+		Handles.DrawLine (point, point + curve.GetDirection (0f) * directionScale);
+
+		for (int i = 1; i <= lineSteps; i++) {
+
+			point = curve.GetPoint (i / (float)lineSteps);
+			Handles.DrawLine (point, point + curve.GetDirection (i / (float)lineSteps) * directionScale);
+
+		}
+
+	}
+
+//		Handles.color = Color.white;
+//		Vector3 lineStart = curve.GetPoint (0f);
+//		Handles.color = Color.green;
+//		Handles.DrawLine(lineStart, lineStart + curve.GetDirection(0f));
+//		for (int i = 1; i <= lineSteps; i++) {
+//			Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
+//			Handles.color = Color.white;
+//			Handles.DrawLine(lineStart, lineEnd);
+//			Handles.color = Color.green;
+//			Handles.DrawLine(lineEnd, lineEnd + curve.GetDirection(i / (float)lineSteps));
+//			lineStart = lineEnd;
+//		}
+//			
+//	}
 
 	private Vector3 ShowPoint (int index){
 
--- project/Assets/Entities/Spline_fun/Spline.cs
+++ project/Assets/Entities/Spline_fun/Spline.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
-public class Curve : MonoBehaviour {
-
+public class Spline : MonoBehaviour {
+	
 	public Vector3[] points;
-
+	
 	public void Reset(){
 		points = new Vector3[] {
 			new Vector3 (1f, 0f, 0f),
@@ -13,18 +13,32 @@ public class Curve : MonoBehaviour {
 			new Vector3 (4f, 0f, 0f)
 		};
 	}
-
+	
 	public Vector3 GetPoint (float t) {
 		return transform.TransformPoint (Bezier.GetPoint (points [0], points [1], points [2], points[3], t));
 	}
-
+	
 	public Vector3 GetVelocity (float t) {
 		return transform.TransformPoint (Bezier.GetFirstDerivative (points [0], points [1], points [2], points[3], t)) -
 			transform.position;
 	}
-
+	
 	public Vector3 GetDirection (float t) {
 		return GetVelocity(t).normalized;
-			
+		
+	}
+
+	public void AddCurve () {
+
+		Vector3 point = points [points.Length - 1];
+		Array.Resize(ref points, points.Length + 3);
+
+		point.x += 1f;
+		points [points.Length - 3] = point;
+		point.x += 1f;
+		points [points.Length - 2] = point;
+		point.x += 1f;
+		points [points.Length - 1] = point;
+
 	}
 }
